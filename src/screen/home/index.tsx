f3a7e19eb01657818e2b80a8ca109dcd9cb06297
Moisés Santana <moisaant@gmail.com>
2023-09-04 03:50:39 -0300

    feat(home): finished

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, View } from 'react-native';
+import { Alert, FlatList, View } from 'react-native';
 import { Header } from '../../components/header';
 import { TaskStatus } from '../../components/task-status';
 import { Task } from '../../components/task';
@@ -12,6 +12,13 @@ export function Home() {
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
   function addTask(task: Todo) {
+    const taskExists = todoList.some((item) => item.description === task.description);
+    if (taskExists) {
+      return Alert.alert(
+        'Tarefa já cadastrada!',
+        'Verifique se essa tarefa já existe na sua lista.'
+      );
+    }
     setTodoList((state) => [...state, task]);
   }
 
@@ -26,8 +33,24 @@ export function Home() {
   }
 
   function removeTask(description: string) {
-    const newList = todoList.filter((task) => task.description !== description);
-    setTodoList(newList);
+    Alert.alert(
+      'Remover item',
+      'Tem certeza que você deseja remover esse item?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel',
+        },
+        {
+          text: 'Sim',
+          onPress: () => {
+            const newList = todoList.filter((task) => task.description !== description);
+            setTodoList(newList);
+            Alert.alert('Item removido com sucesso!');
+          },
+        }
+      ]
+    );
   }
 
   const totalTasks = todoList.length;
