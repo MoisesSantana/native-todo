246891da9f139df29912ae7a96f3dc69f0f24892
Moisés Santana <moisaant@gmail.com>
2023-09-04 01:33:33 -0300

    style(home): create container styles

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -2,10 +2,11 @@ import { View } from 'react-native';
 import { Header } from '../../components/header';
 import { TaskStatus } from '../../components/task-status';
 import { Task } from '../../components/task';
+import { styles } from './styles';
 
 export function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
       <TaskStatus />
       <Task />
--- src/screen/home/styles.ts
+++ src/screen/home/styles.ts
@@ -1,5 +1,9 @@
 import { StyleSheet } from 'react-native';
+import { palette } from '../../styles/theme';
 
 export const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: palette['gray-600'],
+  }
 });
